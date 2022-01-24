d0e174308aeb017fad53730297b735d0a12d915a
nashitshayan <shayan.nashit@gmail.com>
2022-01-24 12:13:08 +0530

    Update practice.js

--- practice/practice.js
+++ practice/practice.js
@@ -142,16 +142,25 @@
 
 // const Person =(name) =>{
 //   const sayMyName= ()=> console.log(`My name is ${name}`);
-//   return {sayMyName};
+//   const sunnahSupremacy = () => console.log(`Let the sunnah for forth!`);
+//   return {sayMyName, sunnahSupremacy};
 // }
+
+// // const Akh = (name)=>{
+// //   const {sayMyName}= Person(name);
+// //   const akhTing = ()=> console.log(`Akh got a nice beard! `);
+// //   return {sayMyName, akhTing};
+// // }
 
 // const Akh = (name)=>{
-//   const {sayMyName}= Person(name);
+//   const prototype = Person(name);
+//   console.dir(prototype)
 //   const akhTing = ()=> console.log(`Akh got a nice beard! `);
-//   return {sayMyName, akhTing};
+//   return Object.assign({}, prototype,{akhTing});
 // }
-
 // const akh1 = Akh('Hamoody');
+// // console.dir(akh1)
+// // akh1.sayMyName();
+// // akh1.akhTing();
 
-// akh1.sayMyName();
-// akh1.akhTing();
+
