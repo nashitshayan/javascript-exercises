7380e90225e65308590cf8c69950969c88098b68
Nashit Shayan <shayan.nashit@gmail.com>
2022-04-19 14:01:21 +0530

    .

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,21 +1,46 @@
-let d= new Date();
-let currentYear= d.getFullYear();
+let d = new Date();
+let currentYear = d.getFullYear();
 
-const findTheOldest = function(ppl) {
+// const findTheOldest = function (ppl) {
+// 	let oldest = ppl.reduce(
+// 		(oldestPerson, currentPerson) => {
+// 			let ageOfCurrentPerson =
+// 				(currentPerson['yearOfDeath'] || currentYear) -
+// 				currentPerson['yearOfBirth'];
 
-    let oldest = ppl.reduce((oldestPerson, currentPerson)=>{
-     
-      let ageOfCurrentPerson= (currentPerson['yearOfDeath'] || currentYear) - currentPerson['yearOfBirth']
-     
-     if(ageOfCurrentPerson> oldestPerson.age)
-       { oldestPerson.age= ageOfCurrentPerson;
-        oldestPerson.name= currentPerson.name}
-  
-        return oldestPerson;
-    }, {name: '', age: 0});
+// 			if (ageOfCurrentPerson > oldestPerson.age) {
+// 				oldestPerson.age = ageOfCurrentPerson;
+// 				oldestPerson.name = currentPerson.name;
+// 			}
 
-    return oldest;
-  }
+// 			return oldestPerson;
+// 		},
+// 		{ name: '', age: 0 },
+// 	);
 
+// 	return oldest;
+// };
+
+const findTheOldest = function (object) {
+	let objAge = {};
+	let arrAge = [];
+	let older;
+	let result = 0;
+	object.map((person) => {
+		const { name } = person;
+		const { yearOfBirth } = person;
+		const { yearOfDeath } = person;
+		arrAge.push(yearOfDeath - yearOfBirth);
+		objAge[name] = yearOfDeath - yearOfBirth;
+
+		older = Math.max(...arrAge);
+	});
+	for (const key in objAge) {
+		if (objAge[key] === older) {
+			result = objAge[key];
+		}
+	}
+	return result;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
